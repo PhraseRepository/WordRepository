9e123f5a88ae746c97de874d44f9f611f7e2dca0
Wyatt Cowley <wycowley@gmail.com>
2021-08-03 20:55:18 -0700

    Basic Sign In Functionality

--- src/components/signin/signin.js
+++ src/components/signin/signin.js
@@ -1,10 +1,13 @@
-import { render } from "@testing-library/react";
 import { useState, useEffect, useRef } from "react";
+import { createClient } from "@supabase/supabase-js";
+import SignInComponent from "./signincomponent";
+const supabase = createClient("https://hiiwioouscmwdgfhobom.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyODA0MTA5NiwiZXhwIjoxOTQzNjE3MDk2fQ.uMF3eAqCD2zgJnJJL6h2rKYSH-d2H6rsGrXGF74X-70");
 
 const SignIn = (params) => {
   return (
     <div>
       <h1>Hello</h1>
+      <SignInComponent></SignInComponent>
     </div>
   );
 };
---
+++ src/components/signin/signincomponent.js
@@ -0,0 +1,46 @@
+import { useState, useEffect, useRef } from "react";
+import { Redirect } from "react-router-dom";
+import { createClient } from "@supabase/supabase-js";
+const supabase = createClient("https://hiiwioouscmwdgfhobom.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyODA0MTA5NiwiZXhwIjoxOTQzNjE3MDk2fQ.uMF3eAqCD2zgJnJJL6h2rKYSH-d2H6rsGrXGF74X-70");
+
+const SignInComponent = (params) => {
+  const [emailEntered, setEmail] = useState("");
+  const [passwordEntered, setPassword] = useState("");
+  const [loggedIn, changeStatus] = useState(false);
+  async function signUp() {
+    const { user, session, error } = await supabase.auth.signUp({
+      email: emailEntered,
+      password: passwordEntered,
+    });
+    console.log("Successful sign up");
+    if (error) {
+      window.alert("Did you mean to sign in?");
+    }
+  }
+  async function signIn() {
+    const { user, session, error } = await supabase.auth.signIn({
+      email: emailEntered,
+      password: passwordEntered,
+    });
+    if (error) {
+      window.alert("Check your email and password and try again!");
+    }
+    if (user) {
+      console.log("Successful sign in ");
+      changeStatus(true);
+    }
+  }
+  return (
+    <div>
+      <h1>Email</h1>
+      <input onChange={(e) => setEmail(e.target.value)}></input>
+      <h1>Password</h1>
+      <input onChange={(e) => setPassword(e.target.value)} type="password"></input>
+      <button onClick={signIn}>Sign In</button>
+      <button onClick={signUp}>Sign Up</button>
+      {loggedIn ? <Redirect to="/" /> : <div />}
+    </div>
+  );
+};
+
+export default SignInComponent;
