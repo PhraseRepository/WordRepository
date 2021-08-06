f51ee06a113b82b8bab0acf4880bf6ac34802f96
SonavAgarwal <42980809+SonavAgarwal@users.noreply.github.com>
2021-08-05 21:33:56 -0700

    home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,30 @@ import PhraseCard from "../components/PhraseCard";
 function Home(props) {
     return (
         <div>
-            <PhraseCard></PhraseCard>
-            <PhraseCard></PhraseCard>
-            <PhraseCard></PhraseCard>
+            <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", padding: "1rem", boxSizing: "border-box" }}>
+                <h1 style={{ fontSize: "5rem", marginBottom: "1rem" }}>
+                    FIND THE PERFECT <span style={{ textDecoration: "underline" }}>ROAST</span>
+                </h1>
+                <input type='text' placeholder='Describe your target...'></input>
+            </div>
+
+            <div style={{ width: "100%", display: "flex", flexDirection: "row" }}>
+                <div className='phrase-column'>
+                    <PhraseCard></PhraseCard>
+                    <PhraseCard></PhraseCard>
+                    <PhraseCard></PhraseCard>
+                </div>
+                <div className='phrase-column'>
+                    <PhraseCard></PhraseCard>
+                    <PhraseCard></PhraseCard>
+                    <PhraseCard></PhraseCard>
+                    <PhraseCard></PhraseCard>
+                </div>
+                <div className='phrase-column'>
+                    <PhraseCard></PhraseCard>
+                    <PhraseCard></PhraseCard>
+                </div>
+            </div>
         </div>
     );
 }
