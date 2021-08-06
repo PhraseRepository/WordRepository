da726b1f595df7de4f4ad06913f112f0a19a4d30
Wyatt Cowley <wycowley@gmail.com>
2021-08-06 13:21:10 -0700

    Fixing auth error

--- src/App.js
+++ src/App.js
@@ -8,16 +8,12 @@ import Navbar from "./components/Navbar";
 import Explore from "./pages/Explore";
 import { createClient } from "@supabase/supabase-js";
 import Search from "./pages/Search";
-const supabase = createClient(
-    "https://hiiwioouscmwdgfhobom.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyODA0MTA5NiwiZXhwIjoxOTQzNjE3MDk2fQ.uMF3eAqCD2zgJnJJL6h2rKYSH-d2H6rsGrXGF74X-70"
-);
+const supabase = createClient("https://hiiwioouscmwdgfhobom.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyODA0MTA5NiwiZXhwIjoxOTQzNjE3MDk2fQ.uMF3eAqCD2zgJnJJL6h2rKYSH-d2H6rsGrXGF74X-70");
 
 function App() {
-    console.log(supabase.auth.user().id);
     return (
         <Router>
-            {supabase.auth.user().id == undefined ? <Redirect to='/signin'></Redirect> : <div></div>}
+            {supabase.auth.user() == undefined ? <Redirect to='/signin'></Redirect> : <div></div>}
             <div style={{ display: "flex", flexDirection: "row" }}>
                 <div className='page'>
                     <Switch>
