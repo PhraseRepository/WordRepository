0c83132c05e22c4263f352e375d5cc0307666233
SonavAgarwal <42980809+SonavAgarwal@users.noreply.github.com>
2021-08-05 22:39:56 -0700

    add navbar options

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,45 @@
 import React from "react";
 
-import { FaBeer } from "react-icons/fa";
+import { FaBeer, FaFire, FaGrinAlt, FaGrinHearts, FaSadCry } from "react-icons/fa";
 
 function Navbar(props) {
     return (
-        <div className='navbar'>
-            <a href='https://google.com' className='navbar-option'>
-                <FaBeer></FaBeer>
-            </a>
-            <a href='https://google.com' className='navbar-option'>
-                <FaBeer></FaBeer>
-            </a>
-            <a href='https://google.com' className='navbar-option'>
-                <FaBeer></FaBeer>
-            </a>
-            <a href='https://google.com' className='navbar-option'>
-                <FaBeer></FaBeer>
-            </a>
-        </div>
+        <>
+            <div className='navbar' style={{ position: "unset" }}>
+                <a href='https://google.com' className='navbar-option'>
+                    <FaFire></FaFire>
+                </a>
+                <a href='https://google.com' className='navbar-option'>
+                    <FaGrinAlt></FaGrinAlt>
+                    <h1>Jokes</h1>
+                </a>
+                <a href='https://google.com' className='navbar-option'>
+                    <FaGrinHearts></FaGrinHearts>
+                    <h1>Pickups</h1>
+                </a>
+                <a href='https://google.com' className='navbar-option'>
+                    <FaSadCry></FaSadCry>
+                    <h1>Roasts</h1>
+                </a>
+            </div>
+            <div className='navbar' style={{ zIndex: 10 }}>
+                <a href='https://google.com' className='navbar-option'>
+                    <FaFire></FaFire>
+                </a>
+                <a href='https://google.com' className='navbar-option'>
+                    <FaGrinAlt></FaGrinAlt>
+                    <h1>Jokes</h1>
+                </a>
+                <a href='https://google.com' className='navbar-option'>
+                    <FaGrinHearts></FaGrinHearts>
+                    <h1>Pickups</h1>
+                </a>
+                <a href='https://google.com' className='navbar-option'>
+                    <FaSadCry></FaSadCry>
+                    <h1>Roasts</h1>
+                </a>
+            </div>
+        </>
     );
 }
 
