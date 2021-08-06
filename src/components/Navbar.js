9e0dfa0753999e9ba9501c22869fa1e0ee6da81f
SonavAgarwal <42980809+SonavAgarwal@users.noreply.github.com>
2021-08-05 22:11:35 -0700

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from "react";
+
+import { FaBeer } from "react-icons/fa";
 
 function Navbar(props) {
     return (
-        <div className="navbar">
-            
+        <div className='navbar'>
+            <a href='https://google.com' className='navbar-option'>
+                <FaBeer></FaBeer>
+            </a>
+            <a href='https://google.com' className='navbar-option'>
+                <FaBeer></FaBeer>
+            </a>
+            <a href='https://google.com' className='navbar-option'>
+                <FaBeer></FaBeer>
+            </a>
+            <a href='https://google.com' className='navbar-option'>
+                <FaBeer></FaBeer>
+            </a>
         </div>
     );
 }
