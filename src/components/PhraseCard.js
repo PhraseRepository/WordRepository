c5268358b9ed56894da9f83b679eff821dca470c
Wyatt Cowley <wycowley@gmail.com>
2021-08-06 20:09:14 -0700

    Saving functionality

--- src/components/PhraseCard.js
+++ src/components/PhraseCard.js
@@ -3,18 +3,24 @@ import React from "react";
 import { useState, useEffect, useRef } from "react";
 
 import { createClient } from "@supabase/supabase-js";
-const supabase = createClient(
-    "https://hiiwioouscmwdgfhobom.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyODA0MTA5NiwiZXhwIjoxOTQzNjE3MDk2fQ.uMF3eAqCD2zgJnJJL6h2rKYSH-d2H6rsGrXGF74X-70"
-);
+const supabase = createClient("https://hiiwioouscmwdgfhobom.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyODA0MTA5NiwiZXhwIjoxOTQzNjE3MDk2fQ.uMF3eAqCD2zgJnJJL6h2rKYSH-d2H6rsGrXGF74X-70");
 
 const PhraseCard = (props) => {
     useEffect(() => {
         console.log(props.object);
-    }, [props]);
+    }, [props.object]);
     async function addSaves() {
-        const { data, error } = await supabase.from("users").select("savedPhrases").match({ userId: supabase.auth.user().id });
-        supabase.from("users").upsert([{ savedPhrases: [] }]);
+        const { data, error } = await supabase.from("users").select("savedPhrases", "id").match({ userId: supabase.auth.user().id });
+        if (data[0].savedPhrases.includes(props.object.id)) {
+            window.alert("You've already saved this item!");
+            return;
+        }
+        var array = [...data[0].savedPhrases, props.object.id];
+        console.log(array);
+        const { data2, error2 } = await supabase
+            .from("users")
+            .update([{ savedPhrases: array }])
+            .match({ userId: supabase.auth.user().id });
     }
     return (
         <>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,33 +2,39 @@ import React from "react";
 import PhraseCard from "../components/PhraseCard";
 import { useState, useEffect, useRef } from "react";
 import { createClient } from "@supabase/supabase-js";
-const supabase = createClient(
-    "https://hiiwioouscmwdgfhobom.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyODA0MTA5NiwiZXhwIjoxOTQzNjE3MDk2fQ.uMF3eAqCD2zgJnJJL6h2rKYSH-d2H6rsGrXGF74X-70"
-);
+const supabase = createClient("https://hiiwioouscmwdgfhobom.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyODA0MTA5NiwiZXhwIjoxOTQzNjE3MDk2fQ.uMF3eAqCD2zgJnJJL6h2rKYSH-d2H6rsGrXGF74X-70");
 
 function Home(props) {
     // State variable that holds the array of rows that are created by you
     const [myPhrases, updatePhrases] = useState([]);
+    const [savedPhrases, updateSavedPhrases] = useState([]);
+
     useEffect(() => {
         console.log("hello");
         // needs to be a separate function for await
         getPersonal();
-    }, []);
+        getSaved();
+    }, [supabase.auth.user()]);
 
     async function getPersonal() {
         // this does correctly get the data from supabase
-        const { data, error } = await supabase
-            .from("pickuplines")
-            .select()
-            .match({ userId: supabase.auth.user()?.id })
-            .order("date", { ascending: true })
-            .limit(4);
+        const { data, error } = await supabase.from("pickuplines").select().match({ userId: supabase.auth.user()?.id }).order("date", { ascending: false }).limit(4);
         // updates the state variable probably where things go wrong
         updatePhrases(data);
         // data.forEach((element) => {
         //     console.log(element);
         // });
+    }
+    async function getSaved() {
+        const { data, error } = await supabase.from("users").select("savedPhrases").match({ userId: supabase.auth.user()?.id });
+        console.log(data);
+        var tempArray = [];
+        for (const savedPostId of data[0].savedPhrases) {
+            const { data: data2, error: error2 } = await supabase.from("pickuplines").select().match({ id: savedPostId });
+            console.log(data2, savedPostId);
+            tempArray.push(data2[0]);
+        }
+        updateSavedPhrases(tempArray);
     }
     return (
         <div>
@@ -60,8 +66,14 @@ function Home(props) {
                 </div>
                 <div className='phrase-column'>
                     <h2>Saved</h2>
-                    {/* <PhraseCard></PhraseCard>
-                    <PhraseCard></PhraseCard> */}
+                    {savedPhrases.map((phraseObject) => {
+                        // the weirdest part is that THIS works, and consoles.logs correctly
+                        console.log(phraseObject);
+                        // but then no phrase cards are being rendered
+
+                        // watch carefully
+                        return <PhraseCard object={phraseObject} key={phraseObject.id}></PhraseCard>;
+                    })}
                 </div>
             </div>
         </div>
